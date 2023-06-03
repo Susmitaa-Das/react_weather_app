e52e2d5353ff91d4569d66bf85bbcf34cc2b6b61
Susmitaa-Das <susmitadas2003hasi2021@gmail.com>
2023-06-03 20:31:22 +0530

    react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  import { useEffect } from 'react';
+  import './App.css';
+  
+  const API_URL = 'https://api.openweathermap.org/data/2.5/weather?';
+  
+  const App = () => {
+    const searchCity = async (city) => {
+      const response = await fetch(`${API_URL}q=${city}&appid=6a88ec3937b5a413c685aea098f58e29`);
+      const data = await response.json();
+      console.log(data);
+    };
+  
+    useEffect(() => {
+      searchCity('Kolkata');
+    }, []);
+  
+    return (
+      <div>
+        <h1>Weather</h1>
+      </div>
+    );
+  };
+  
+  export default App;
+  
