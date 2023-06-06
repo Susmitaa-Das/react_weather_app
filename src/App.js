24f0e5f80cb0ea9801e8512d88d5d8918ec3c182
Susmitaa-Das <susmitadas2003hasi2021@gmail.com>
2023-06-06 23:39:56 +0530

    react app

--- src/App.js
+++ src/App.js
@@ -54,53 +54,40 @@
 
 //   export default App;
 
-import React, { useEffect, useState } from 'react'
 
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import SearchIcon from './search.svg'
 
 const API_URL = 'https://api.openweathermap.org/data/2.5/weather?'
 
-// const city1 = {
-//   temp: 307.12,
-//   feels_like: 314.12,
-//   temp_min: 307.12,
-//   temp_max: 307.12,
-//   pressure: 1001,
-//   humidity: 62,
-// }
-
 const WeatherApp = () => {
   const [temperature, setTemperature] = useState(null)
+  const [searchValue, setSearchValue] = useState('')
 
+  const searchCity = async (city) => {
+    const response = await fetch(
+      `${API_URL}q=${city}&appid=6a88ec3937b5a413c685aea098f58e29`
+    )
+    const data = await response.json()
+      
+    const temp = data.main.temp
+    const temperatureCelsius = Math.round(temp - 273.15)
+    setTemperature(temperatureCelsius)
+  }
 
   useEffect(() => {
-    const searchCity = async (city) => {
-      const response = await fetch(
-        `${API_URL}q=${city}&appid=6a88ec3937b5a413c685aea098f58e29`
-      )
-      const data = await response.json()
-      console.log(data)
-      
-      const temp = data.main.temp
-      const temperatureCelsius = Math.round(temp - 273.15)
-      setTemperature(temperatureCelsius)
-    }
-
-    // Extract the temperature value from the API response
-
-    // Convert temperature from Kelvin to Celsius or Fahrenheit if needed
-
-    // const temperatureFahrenheit = (temp - 273.15) * 9/5 + 32;
-
-    // Set the temperature state
+    // Search the default city on initial load
     searchCity('Kolkata')
   }, [])
+
+  const handleSearch = () => {
+    searchCity(searchValue)
+  }
 
   if (temperature === null) {
     return <div>Loading...</div>
   }
-  // <p>The current temperature is: {temperature.toFixed(2)}°C</p>
 
   return (
     <div className="app">
@@ -108,13 +95,13 @@ const WeatherApp = () => {
       <div className="search">
         <input
           placeholder="Search City"
-          value={'Search city'}
-          onChange={(e) => {e.target.value}}
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
         />
         <img
           src={SearchIcon}
           alt="search"
-          onClick={() => {}}
+          onClick={handleSearch}
         />
       </div>
 
@@ -130,3 +117,4 @@ const WeatherApp = () => {
 }
 
 export default WeatherApp
+
