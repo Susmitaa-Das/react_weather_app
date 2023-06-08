462f18b8097ebe2343e0c30e278450565a2c0d2e
Susmitaa-Das <susmitadas2003hasi2021@gmail.com>
2023-06-09 01:16:43 +0530

    react app

--- src/App.js
+++ src/App.js
@@ -1,70 +1,15 @@
-//   import { useEffect } from 'react';
-//   import './App.css';
-//   import SearchIcon from './search.svg'
-
-//   const API_URL = 'https://api.openweathermap.org/data/2.5/weather?';
-
-//   const city1 = {
-//     "temp": 307.12,
-//     "feels_like": 314.12,
-//     "temp_min": 307.12,
-//     "temp_max": 307.12,
-//     "pressure": 1001,
-//     "humidity": 62
-// }
-
-//   const App = () => {
-
-//     const searchCity = async (city) => {
-//       const response = await fetch(`${API_URL}q=${city}&appid=6a88ec3937b5a413c685aea098f58e29`);
-//       const data = await response.json();
-//       console.log(data);
-//     };
-
-//     useEffect(() => {
-//       searchCity('Kolkata');
-
-//     }, []);
-
-//     return (
-//       <div className='app'>
-//         <h1>Weather</h1>
-//         <div className='search'>
-//           <input
-//           placeholder='Search City'
-//           value={'kolkata'}
-//           onChange={()=>{}}
-//           />
-//           <img
-//           src={SearchIcon}
-//           alt='search'
-//           onClick={()=>{}}
-//           />
-//         </div>
-//         <div>
-//           <div>
-//             <div>
-
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   };
-
-//   export default App;
-
-
+  
 import React, { useEffect, useState } from 'react'
 import './App.css'
 import SearchIcon from './search.svg'
+import CloudIcon from './cloud.svg'
 
 const API_URL = 'https://api.openweathermap.org/data/2.5/weather?'
 
 const WeatherApp = () => {
   const [temperature, setTemperature] = useState(null)
   const [humidity, setHumidity] = useState(null)
-  const [feels, setFeels] =useState(null)
+  const [feels, setFeels] = useState(null)
   const [searchValue, setSearchValue] = useState('')
 
   const searchCity = async (city) => {
@@ -87,7 +32,6 @@ const WeatherApp = () => {
     const feel = data.main.feels_like
     const feelsCelsius = Math.round(feel - 273.15)
     setFeels(feelsCelsius)
-
   }
 
   useEffect(() => {
@@ -112,19 +56,18 @@ const WeatherApp = () => {
           value={searchValue}
           onChange={(e) => setSearchValue(e.target.value)}
         />
-        <img
-          src={SearchIcon}
-          alt="search"
-          onClick={handleSearch}
-        />
+        <img src={SearchIcon} alt="search" onClick={handleSearch} />
       </div>
 
       <div>
         <div>
           <div>
-            <p className='temp'>The current temperature is: {temperature.toFixed(2)}°C</p>
-            <p>Humidity:{humidity}%</p>
-            <p>Feels_like:{feels.toFixed(2)}°C</p>
+            <p className="temp">
+            <img src={CloudIcon} alt="cloud" className="cloud-icon" />
+             {temperature.toFixed(0)}°C
+            </p>
+            <p className='feel'>Feels like {feels.toFixed(0)}°C.</p>
+            <p className='humi'>Humidity:{humidity}%</p>
           </div>
         </div>
       </div>
@@ -133,4 +76,3 @@ const WeatherApp = () => {
 }
 
 export default WeatherApp
-
