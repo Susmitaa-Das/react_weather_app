d015e306b9d87c3fe31d73c6867b7211608431d7
Susmitaa-Das <susmitadas2003hasi2021@gmail.com>
2023-06-24 23:55:15 +0530

    Added Forecast Card css

--- src/Components/ForecastCard.jsx
+++ src/Components/ForecastCard.jsx
@@ -1,88 +1,88 @@
- import React, { useState } from 'react'
- import Forecast from './Forecast'
+import React, { useState } from 'react'
+import Forecast from './Forecast'
 
- const ForecastCard = ({ list , temperature, humidity }) => {
-   const [isExpanded, setIsExpanded] = useState(false)
- 
+const ForecastCard = ({ list, temperature, humidity }) => {
+  // const [isExpanded, setIsExpanded] = useState(false)
 
-   const toggleExpand = () => {
-     setIsExpanded(!isExpanded)
-   }
-  
+  // const toggleExpand = () => {
+  //   setIsExpanded(!isExpanded)
+  // }
 
-//   return (
-//     <div className="forecast">
-//       {list.map((item) => {
-//         const date = new Date(item.dt * 1000)
-//         const options = {
-//           weekday: 'long',
-//           year: 'numeric',
-//           month: 'long',
-//           day: 'numeric',
-//           hour: 'numeric',
-//           minute: 'numeric',
-//           second: 'numeric',
-//           timeZone: 'UTC',
-//         }
+  //   return (
+  //     <div className="forecast">
+  //       {list.map((item) => {
+  //         const date = new Date(item.dt * 1000)
+  //         const options = {
+  //           weekday: 'long',
+  //           year: 'numeric',
+  //           month: 'long',
+  //           day: 'numeric',
+  //           hour: 'numeric',
+  //           minute: 'numeric',
+  //           second: 'numeric',
+  //           timeZone: 'UTC',
+  //         }
 
+  //         const formattedDate = date.toLocaleDateString('en-US', options)
 
-//         const formattedDate = date.toLocaleDateString('en-US', options)
-
-//         return (
-//           <div key={item.dt}>
-//             <p>5 Days forecast</p>
-//             <p>{formattedDate}</p>
-//             <button className="extend" onClick={toggleExpand}>
-//               {isExpanded ? '▼' : '▲'}
-//             </button>
-//             {isExpanded && (
-//               <div>
-//                <p>Temperature: {temperature}°C</p>
-//                 <p>Humidity: {humidity}%</p>
-//               </div>
-//             )}
-//           </div>
-//         )
-//       })}
-//     </div>
-//   )
-
+  //         return (
+  //           <div key={item.dt}>
+  //             <p>5 Days forecast</p>
+  //             <p>{formattedDate}</p>
+  //             <button className="extend" onClick={toggleExpand}>
+  //               {isExpanded ? '▼' : '▲'}
+  //             </button>
+  //             {isExpanded && (
+  //               <div>
+  //                <p>Temperature: {temperature}°C</p>
+  //                 <p>Humidity: {humidity}%</p>
+  //               </div>
+  //             )}
+  //           </div>
+  //         )
+  //       })}
+  //     </div>
+  //   )
 
   //...
- return (
-   <div className="forecast">
-     {list.map((item) => {
-       const date = new Date(item.dt * 1000);
-       const formattedTime = date.toLocaleString('en-US', {
-         weekday:'long',
-         hour: 'numeric',
-         minute: 'numeric',
-         day: 'numeric',
-         hour12: true,
-       });
-       return (
-         <div key={item.dt}>
-           <p>{formattedTime}</p>
-            <button className="extend" onClick={toggleExpand}>
-             {isExpanded ? '▼' : '▲'}
-           </button> 
-          
+  return (
+    <div className="forecast">
+      {list.map((item) => {
+        const date = new Date(item.dt * 1000)
+        const formattedTime = date.toLocaleString('en-US', {
+          weekday: 'long',
+          hour: 'numeric',
+          minute: 'numeric',
+          day: 'numeric',
+          hour12: true,
+        })
+        return (
+          <div key={item.dt}>
+            <div className='time'>
+            <p>{formattedTime}</p>
+            </div>
+            {/* <button className="extend" onClick={toggleExpand}>
+              {isExpanded ? '▼' : '▲'}
+            </button> */}
+            {/*           
            {isExpanded && (
              <div>
               <p> 5 days Forecast</p>
                <p>Temperature: {item.temperatureCelsius}°C</p>
                <p>Humidity: {humidity}%</p>
              </div>
-           )}
-           
-         </div>
-       );
-     })}
-   </div>
-   
- );
-// ...
+           )} */}
 
+            <div>
+              <p>Temperature: {item.temperatureCelsius}°C</p>
+              <p>Humidity: {humidity}%</p>
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
+  // ...
 }
 
 export default ForecastCard
